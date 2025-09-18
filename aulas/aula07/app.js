8890a6f75d55e08495a4a7551cc0886d300bdd50
unknown <jhonata.matos@iesb.edu.br>
2025-09-18 10:50:50 -0300

    codigos da aula 7

--- aulas/aula07/app.js
+++ aulas/aula07/app.js
@@ -12,5 +12,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+app.get('/tarefas', (req, res) => {
+    res.send("Ok")
+    
+});
+
+app.get("/tarefas", (req ,res) => {
+    res.json(tarefas);
+});
+
+app.post("/tarefas", (req ,res) => {
+    const novatarefa = {
+        id: tarefas.length + 1,
+        nome: req.body.nome,
+        concluida: false,
+    }
+    res.status(201).json(novatarefa);
+})
+
+app.get('/tarefas/:id', (req, res) => {
+    res.json({});
+})
 
 module.exports = app;
